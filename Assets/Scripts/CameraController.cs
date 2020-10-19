2ff21a764d34ddf40576407bc108f28be6732070
BoraCakmaz <bora.cakmaz@gmail.com>
2020-10-19 16:47:28 +0300

    Update CameraController.cs

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -13,7 +13,7 @@ public class CameraController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = lookAt.position + offset;
+        transform.position = Time.deltaTime * lookAt.position + offset ;
     }
 
     
